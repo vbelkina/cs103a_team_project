b4b2b17b76a99988c9d1a18a899cb6a8dca0b05c
Daniel Mints <67702241+DanMint@users.noreply.github.com>
2023-03-24 01:55:25 -0400

    Update tracker.py

--- pa03/tracker.py
+++ pa03/tracker.py
@@ -1,16 +1,19 @@
 from transaction import trackerList
+from category import categoriesList
 import sys
 
 def print_usage():
     ''' print an explanation of how to use this command '''
     print('''usage:
             type quit (description: to end the program)
-            type showall (description: for all transactions)
-            type add amount category date description (description: to add a new transaction)
+            type showall_categories (description: show all categories)
+            type add_category (description: to add a new category)
+            type showall_transactions(description: for all transactions)
+            type add_transaction amount category date description (description: to add a new transaction)
             '''
             )
 
-def print_todos(todos):
+def print_transactions(todos):
     if len(todos)==0:
         print('no tasks to print')
         return
@@ -21,15 +24,36 @@ def print_todos(todos):
         values = tuple(item.values()) #(rowid,title,desc,completed)
         print("%-30s %-30s %-30s %-30s %-30s"%values)
 
+def print_categories(todos):
+    if len(todos)==0:
+        print('no tasks to print')
+        return
+    print('\n')
+    print("%-30s %-30s %-30s %-30s %-30s"%('item #', 'category',))
+    print('-'*150)
+    for item in todos:
+        values = tuple(item.values()) #(rowid,title,desc,completed)
+        print("%-30s %-30s "%values)
+
 def process_args(arglist):
-    todolist = trackerList()
+    track_list = trackerList()
+    category_list = categoriesList()
     if arglist==[]:
         print_usage()
-    elif arglist[0]=="showall":
-        print_todos(todos = todolist.selectAll())
-    elif arglist[0]=='add':
-            todo = {'amount':arglist[1],'category':arglist[2], 'date':arglist[3], 'description':arglist[4]}
-            todolist.add(todo)
+        
+    elif arglist[0]=="showall_transactions":
+        print_transactions(todos = track_list.selectAll())
+
+    elif arglist[0]=="showall_categories":
+        print_categories(todos = category_list.selectAll())
+
+    elif arglist[0]=='add_transaction':
+        todo = {'amount':arglist[1],'category':arglist[2], 'date':arglist[3], 'description':arglist[4]}
+        track_list.add(todo)
+
+    elif arglist[0]=='add_category':
+        category_list.add({'categories_name':arglist[1]})
+
     else:
         print(arglist,"is not implemented")
         print_usage()
@@ -43,8 +67,13 @@ def toplevel():
             args = input("command> ").split(' ')
             if args[0]=='quit':
                 break
-            if args[0]=='add':
-                args = ['add', args[1], args[2], args[3], args[4]]
+
+            elif args[0]=='add_transaction':
+                args = ['add_transaction', args[1], args[2], args[3], args[4]]
+
+            elif args[0]=='add_category':
+                args = ['add_category', args[1]]
+
             process_args(args)
     else:
         args = sys.argv[1:]
