71857c0eaa36a43586383d111cc87f29e2c4d5aa
Daniel Mints <67702241+DanMint@users.noreply.github.com>
2023-03-23 23:54:21 -0400

    Create transaction.py

---
+++ pa03/transaction.py
@@ -0,0 +1,31 @@
+import sqlite3
+import os
+
+def toDict(t):
+    print('t='+str(t))
+    tracker = {'rowid':t[0], 'amount':t[1], 'category':t[2], 'date':t[3], 'description':t[4]}
+    return tracker
+
+class trackerList():
+    def __init__(self):
+        self.runQuery('''CREATE TABLE IF NOT EXISTS tracker
+                    (amount text, category text, date text, description text)''',())
+    
+    def selectAll(self):
+        return self.runQuery("SELECT rowid,* from tracker",())
+
+    def add(self,item):
+        return self.runQuery("INSERT INTO tracker VALUES(?,?,?,?)",(item['amount'],item['category'],item['date'],item['description']))
+
+    def runQuery(self,query,tuple):
+        ''' return all of the uncompleted tasks as a list of dicts.'''
+        con= sqlite3.connect(os.getenv('HOME')+'/tracker.db')
+        cur = con.cursor() 
+        cur.execute(query,tuple)
+        tuples = cur.fetchall()
+        con.commit()
+        con.close()
+        return [toDict(t) for t in tuples]
+
+    
+        
