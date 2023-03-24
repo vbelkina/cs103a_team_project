0b7871c902ae84a4d7f317d650e6b58628e43048
Daniel Mints <67702241+DanMint@users.noreply.github.com>
2023-03-24 00:17:40 -0400

    Update transaction.py

--- pa03/transaction.py
+++ pa03/transaction.py
@@ -1,31 +1,56 @@
-import sqlite3
-import os
+from transaction import trackerList
+import sys
 
-def toDict(t):
-    print('t='+str(t))
-    tracker = {'rowid':t[0], 'amount':t[1], 'category':t[2], 'date':t[3], 'description':t[4]}
-    return tracker
+def print_usage():
+    ''' print an explanation of how to use this command '''
+    print('''usage:
+            type quit (description: to end the program)
+            type showall (description: for all transactions)
+            type add amount category date description (description: to add a new transaction)
+            '''
+            )
 
-class trackerList():
-    def __init__(self):
-        self.runQuery('''CREATE TABLE IF NOT EXISTS tracker
-                    (amount text, category text, date text, description text)''',())
+def print_todos(todos):
+    if len(todos)==0:
+        print('no tasks to print')
+        return
+    print('\n')
+    print("%-30s %-30s %-30s %-30s %-30s"%('item #', 'amount', 'category', 'date', 'description'))
+    print('-'*150)
+    for item in todos:
+        values = tuple(item.values()) #(rowid,title,desc,completed)
+        print("%-30s %-30s %-30s %-30s %-30s"%values)
+
+def process_args(arglist):
+    todolist = trackerList()
+    if arglist==[]:
+        print_usage()
+    elif arglist[0]=="showall":
+        print_todos(todos = todolist.selectAll())
+    elif arglist[0]=='add':
+            todo = {'amount':arglist[1],'category':arglist[2], 'date':arglist[3], 'description':arglist[4]}
+            todolist.add(todo)
+    else:
+        print(arglist,"is not implemented")
+        print_usage()
+
+
+def toplevel():
+    if len(sys.argv)==1:
+        print_usage()
+        args = []
+        while args!=['']:
+            args = input("command> ").split(' ')
+            if args[0]=='quit':
+                break
+            if args[0]=='add':
+                args = ['add', args[1], args[2], args[3], args[4]]
+            process_args(args)
+    else:
+        args = sys.argv[1:]
+        process_args(args)
+        print('-'*40+'\n'*3)
+
     
-    def selectAll(self):
-        return self.runQuery("SELECT rowid,* from tracker",())
 
-    def add(self,item):
-        return self.runQuery("INSERT INTO tracker VALUES(?,?,?,?)",(item['amount'],item['category'],item['date'],item['description']))
-
-    def runQuery(self,query,tuple):
-        ''' return all of the uncompleted tasks as a list of dicts.'''
-        con= sqlite3.connect(os.getenv('HOME')+'/tracker.db')
-        cur = con.cursor() 
-        cur.execute(query,tuple)
-        tuples = cur.fetchall()
-        con.commit()
-        con.close()
-        return [toDict(t) for t in tuples]
-
-    
-        
+toplevel()
