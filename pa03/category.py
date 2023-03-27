1b969b83a0dacfcd34b003f7584345977af7b565
Daniel Mints <67702241+DanMint@users.noreply.github.com>
2023-03-26 23:07:16 -0400

    Update category.py

--- pa03/category.py
+++ pa03/category.py
@@ -1,21 +1,26 @@
 import sqlite3
 import os
 
+# added by Daniel
 def to_dict(t):
     '''Converts a tuple to a dictionary.'''
     categories = {'rowid': t[0], 'categories_name': t[1]}
     return categories
 
+# added by Daniel
 class CategoriesList:
+    # added by Daniel
     '''A class representing a list of categories.'''
     def __init__(self):
         '''Initializes the CategoriesList object.'''
         self.run_query('''CREATE TABLE IF NOT EXISTS categories (categories_name text)''',())
-
+        
+    # added by Daniel
     def selectAll(self):
         '''Returns all rows from the categories table as a list of dictionaries.'''
         return self.run_query("SELECT rowid,* from categories",())
-
+    
+    # added by Daniel
     def add(self, item):
         '''Inserts a new row into the categories table.'''
         return self.run_query("INSERT INTO categories VALUES(?)", (item['categories_name'],))
@@ -23,7 +28,8 @@ class CategoriesList:
     # added by Veronika
     def modify(self, item_name, category_name):
         return self.runQuery("UPDATE categories SET categories_name=(?) WHERE categories_name=(?)",(category_name, item_name))
-
+    
+    # added by Daniel
     def run_query(self, query, ntuple):
         '''Executes a SQL query and returns the result as a list of dictionaries.'''
         con = sqlite3.connect(os.getenv("HOME")+'/categories.db')
